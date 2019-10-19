017596d8701586bb6c0581c0994b7e9053f747f5
Nathan Duckett <nduckett13@gmail.com>
2019-10-19 15:14:21 +1300

    Converting to background tasks, requiring user input for share information

--- download_build.sh
+++ download_build.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
-NETWORK_SHARE_IP=192.168.1.39
-SMB_SHARE_USERNAME=natha
+read -p 'Network Share IP:' NETWORK_SHARE_IP
+read -p 'Network Share Username:' SMB_SHARE_USERNAME
 
-apt install cifs-utils docker docker-compose -y
+echo "Installing Dependencies"
+apt install cifs-utils docker docker-compose -y &
+echo "Finished Install Dependencies"
 
 # Mount SMB Network Shares
-mkdir /mnt/download
-mount -t cifs -o user=$SMB_SHARE_USERNAME //$NETWORK_SHARE_IP/Download /mnt/download
-mkdir -p ~/vpnconfig/openvpn/
-cp /mnt/download/nl-aes-128-cbc-udp-dns.ovpn ~/vpnconfig/openvpn/
-cp /mnt/download/pia_creds.txt ~/vpnconfig/openvpn/
+echo "Mounting shares and setting up configs"
+mkdir /mnt/download &
+mount -t cifs -o user=$SMB_SHARE_USERNAME //$NETWORK_SHARE_IP/Download /mnt/download &
+mkdir -p ~/vpnconfig/openvpn/ &
+cp /mnt/download/nl-aes-128-cbc-udp-dns.ovpn ~/vpnconfig/openvpn/ &
+cp /mnt/download/pia_creds.txt ~/vpnconfig/openvpn/ &
 
+echo "Building qBittorrent Docker"
 cd docker-builds/qBittorrent-only/
 docker-compose up -d
