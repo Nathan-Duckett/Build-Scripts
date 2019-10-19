8d22428b224fccf266847611a59ef598f48d565f
Nathan Duckett <nduckett13@gmail.com>
2019-10-19 14:47:11 +1300

    Fixing directory craetion for vpn configuration

--- download_build.sh
+++ download_build.sh
@@ -7,9 +7,9 @@ apt install cifs-utils docker docker-compose -y
 # Mount SMB Network Shares
 mkdir /mnt/download
 mount -t cifs -o user=$SMB_SHARE_USERNAME //$NETWORK_SHARE_IP/Download /mnt/download
-mkdir ~/vpnconfig/openvpn
-cp /mnt/download/nl-aes-128-cbc-udp-dns.ovpn ~/vpnconfig/openvpn
-cp /mnt/download/pia_creds.txt ~/vpnconfig/openvpn
+mkdir -p ~/vpnconfig/openvpn/
+cp /mnt/download/nl-aes-128-cbc-udp-dns.ovpn ~/vpnconfig/openvpn/
+cp /mnt/download/pia_creds.txt ~/vpnconfig/openvpn/
 
 cd docker-builds/qBittorrent-only/
 docker-compose up -d
