1aea932a24627e73d680e2a069d493a0a6d5806d
Nathan Duckett <nduckett13@gmail.com>
2019-10-19 15:18:12 +1300

    Piping output to /dev/null

--- download_build.sh
+++ download_build.sh
@@ -3,16 +3,16 @@ read -p 'Network Share IP:' NETWORK_SHARE_IP
 read -p 'Network Share Username:' SMB_SHARE_USERNAME
 
 echo "Installing Dependencies"
-apt install cifs-utils docker docker-compose -y &
+apt install cifs-utils docker docker-compose -y & > /dev/null
 echo "Finished Install Dependencies"
 
 # Mount SMB Network Shares
 echo "Mounting shares and setting up configs"
-mkdir /mnt/download &
-mount -t cifs -o user=$SMB_SHARE_USERNAME //$NETWORK_SHARE_IP/Download /mnt/download &
+mkdir /mnt/download & > /dev/null
+mount -t cifs -o user=$SMB_SHARE_USERNAME //$NETWORK_SHARE_IP/Download /mnt/download
 mkdir -p ~/vpnconfig/openvpn/ &
-cp /mnt/download/nl-aes-128-cbc-udp-dns.ovpn ~/vpnconfig/openvpn/ &
-cp /mnt/download/pia_creds.txt ~/vpnconfig/openvpn/ &
+cp /mnt/download/nl-aes-128-cbc-udp-dns.ovpn ~/vpnconfig/openvpn/ & > /dev/null
+cp /mnt/download/pia_creds.txt ~/vpnconfig/openvpn/ & > /dev/null
 
 echo "Building qBittorrent Docker"
 cd docker-builds/qBittorrent-only/
