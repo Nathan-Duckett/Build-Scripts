50d6567b18c7786809cb63d3989f0700c58cd77d
Nathan Duckett <nduckett13@gmail.com>
2019-10-27 00:46:53 +1300

    Persistent Network shares on reboot

--- media-docker.sh
+++ media-docker.sh
@@ -18,11 +18,21 @@ apt install cifs-utils docker docker-compose -y > /dev/null
 # Mount SMB Network Shares
 echo "Mounting network shares"
 
+cat > ~/.smbcredentials << EOF
+username=$SMB_SHARE_USERNAME
+passsword=$SMB_SHARE_PASSWORD
+EOF
+chmod 600 ~/.smbcredentials
+
 mkdir /mnt/download > /dev/null
-mount -t cifs -o "user=$SMB_SHARE_USERNAME,password=$SMB_SHARE_PASSWORD" //$NETWORK_SHARE_IP/Download /mnt/download
+mount -t cifs -o "user=$SMB_SHARE_USERNAME,password=$SMB_SHARE_PASSWORD" //$NETWORK_SHARE_IP/Download /mnt/download > /dev/null
 
 mkdir /mnt/videos > /dev/null
-mount -t cifs -o "user=$SMB_SHARE_USERNAME,password=$SMB_SHARE_PASSWORD" //$NETWORK_SHARE_IP/Videos /mnt/videos
+mount -t cifs -o "user=$SMB_SHARE_USERNAME,password=$SMB_SHARE_PASSWORD" //$NETWORK_SHARE_IP/Videos /mnt/videos > /dev/null
+
+echo "//$NETWORK_SHARE_IP/Download /mnt/download cifs credentials=/home/$USER/.smbcredentials,iocharset=utf8,sec=ntlm 0 0" >> /etc/fstab
+echo "//$NETWORK_SHARE_IP/Videos /mnt/videos cifs credentials=/home/$USER/.smbcredentials,iocharset=utf8,sec=ntlm 0 0" >> /etc/fstab
+
 
 # Create config folder locations
 mkdir ~/.media-config/ > /dev/null
@@ -35,4 +45,6 @@ mkdir -p ~/.transcode/temp > /dev/null
 
 echo "Building Docker Containers"
 cd docker-builds/media/
-docker-compose up -d
+docker-compose up -d > /dev/null
+
+echo "Installation Complete. Enjoy the server"
