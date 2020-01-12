d3783da47026a002731fa85cbaf1502ba2877797
Nathan Duckett <nduckett13@gmail.com>
2020-01-12 18:24:31 +1300

    Adding script to install ZSH shell and configure

---
+++ ubuntu1804/install-shell.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+### README:
+### When using this script for a WSL instance or to access on windows you must
+### install powerline fonts to support the console. Visit https://github.com/powerline/fonts.git
+### to get started. For windows you can use:
+### git clone https://github.com/powerline/fonts.git then .\install.ps1
+
+
+apt install zsh git fonts-powerline
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark --output ~/.dircolors
+cat >> ~/.zshrc << EOF
+source ~/.aliases
+
+# Prevent User prompt on local machine
+prompt_context(){}
+
+# Stop WSL Bell sounds
+unsetopt beep
+
+# Set colours for LS
+eval `dircolors ~/.dircolors`
+EOF
