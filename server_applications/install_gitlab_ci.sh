13122407832a70610542eada8c1e09c5428431f5
Nathan Duckett <nduckett13@gmail.com>
2020-05-11 21:40:23 +1200

    Add basic start for Gitlab CI Runner installation

---
+++ server_applications/install_gitlab_ci.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+URL=""
+TOKEN=""
+
+curl -LJO https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb
+sudo dpkg -i gitlab-runner_amd64.deb
+
+sudo gitlab-runner register \
+  --non-interactive \
+  --url "$URL" \
+  --registration-token "$TOKEN" \
+  --executor "shell" \
+  --description "[Anton] Gitlab shell runner to execute on server" \
+  --tag-list "shell,java,python" \
+  --run-untagged="true" \
+  --locked="true" \
+  --access-level="protected"
