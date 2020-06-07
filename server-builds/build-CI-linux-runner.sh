9ec151069e9382d8dcc0c92b3edd6b35a6953193
Nathan Duckett <nduckett13@gmail.com>
2020-06-07 23:22:51 +1200

    Add script to add cron-job for Linux CI runner

--- server-builds/build-CI-linux-runner.sh
+++ server-builds/build-CI-linux-runner.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-if [ "$#" -ne 2 ]; then
-    echo "Incorrect usage; Should be: install_gitlab_ci.sh <url> <token>"
+if [ "$#" -ne 4 ]; then
+    echo "Incorrect usage; Should be: install_gitlab_ci.sh <url> <token> <PAT> <Project_ID>"
     exit 1
 fi
 
 URL="$1"
 TOKEN="$2"
+PAT="$3"
+PROJECT_ID="$4"
 
 # Update and prepare
 
@@ -17,7 +19,7 @@ sudo apt install openjdk-11-jdk maven git curl htop -y
 # Workaround to require no ssl on git repos
 git config --global http.sslverify false
 
-CI_PWD="/home/nathan/GitlabCI"
+CI_PWD="$HOME/GitlabCI"
 
 # Ensure CI_PWD exists
 mkdir -p $CI_PWD
@@ -33,17 +35,49 @@ sudo gitlab-runner register \
   --url "$URL" \
   --registration-token "$TOKEN" \
   --executor "shell" \
-  --description "Nathan-Server GitlabCI Kubuntu20.04 runner with KDE" \
-  --tag-list "nathan,linux" \
+  --description "$USER-$HOSTNAME Gitlab CI Runner" \
+  --tag-list "$USER,linux" \
   --run-untagged="true" \
   --locked="true" \
   --access-level="not_protected"
 
 # Install as a service and start
 sudo gitlab-runner uninstall
-sudo gitlab-runner install --user "nathan" --working-directory "$CI_PWD"
+sudo gitlab-runner install --user "$USER" --working-directory "$CI_PWD"
 
 sudo gitlab-runner start
 
 # Restart just in case didn't correctly register.
 sudo gitlab-runner restart
+
+
+# Cronjob of management tools
+# GitlabTimeCounter from Tim Salisbury for counting up all time spent
+git clone https://github.com/TimSalisbury/GitlabTimeCounter
+mv GitlabTimeCounter $HOME/GitlabTimeCounter
+cat << EOF > $HOME/GitlabTimeCounter/config.ini
+[AUTHENTICATION]
+GitLabServer = https://gitlab.ecs.vuw.ac.nz
+PersonalAccessToken = $PAT
+
+[PROJECT]
+ID = $PROJECT_ID
+SprintMilestonePrefix = Sprint
+EOF
+
+# Gitlab-Wiki-Uploader to post this information into the wiki
+git clone https://github.com/Nathan-Duckett/Gitlab-Wiki-Updater.git
+mv Gitlab-Wiki-Updater $HOME/Gitlab-Wiki-Updater
+cat << EOF > $HOME/Gitlab-Wiki-Updater/config.yaml
+rootURI: https://gitlab.ecs.vuw.ac.nz
+PAT: $PAT
+projectID: $PROJECT_ID
+wikiSlug: Time-Tracking-by-Team-Member
+EOF
+
+# https://stackoverflow.com/questions/4880290/how-do-i-create-a-crontab-through-a-script
+# Set to run at midnight nightly
+(crontab -l 2>/dev/null; echo "0 0 * * * python3 $HOME/GitlabTimeCounter/TimeCounter.py | $HOME/Gitlab-Wiki-Updater/upload.py") | crontab -
+
+# Run upload lab times now
+python3 $HOME/GitlabTimeCounter/TimeCounter.py | $HOME/Gitlab-Wiki-Updater/upload.py
